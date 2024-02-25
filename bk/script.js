cac9448baf0de763339a5cec578ab613a0d1e985
Seiji Enomoto <iwillbestandbyme@gmail.com>
2024-02-25 12:51:30 +0900

    #メッセージ機能追加、socket io使用でリアルタイム通信実装。

---
+++ bk/script.js
@@ -0,0 +1,41 @@
+// カウンターの初期値をローカルストレージから取得する
+let counterValue = localStorage.getItem('counterValue') ? parseInt(localStorage.getItem('counterValue')) : 0;
+
+// カウンターの初期値を表示する
+updateCounter();
+
+// カウンターの更新
+function updateCounter() {
+    // カウンターを表示する要素を取得し、カウンターの値を設定
+    const counterElement = document.getElementById('counter');
+    counterElement.textContent = counterValue;
+}
+
+// インクリメント
+function increment() {
+    counterValue++;
+    updateCounter();
+    // カウンターの値をローカルストレージに保存
+    localStorage.setItem('counterValue', counterValue.toString());
+}
+
+// デクリメント
+function decrement() {
+    if (counterValue > 0) {
+        counterValue--;
+        updateCounter();
+        // カウンターの値をローカルストレージに保存
+        localStorage.setItem('counterValue', counterValue.toString());
+    }
+}
+
+// const socket = io();
+
+// Socket.ioからのカウンターの更新を処理
+socket.on('counterUpdate', (value) => {
+    counterValue = value;
+    updateCounter();
+});
+
+// 以下、日時の表示やカウントダウンの更新などの処理を追加する
+
